fb4d8cad7c4d93abe2867fb42091f1d4f09d596a
RahulRaj <jokingboyrahul@gmail.com>
2021-08-14 18:04:27 +0530

    [Rahul Raj] : Succesfully added TODO LIST in databases

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+from werkzeug.datastructures import RequestCacheControl
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///TODO.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class TOdo(db.Model):
+class ToDo(db.Model):
     sno =db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(200), nullable = False)
     desc = db.Column(db.String(2000), nullable = False)
@@ -18,9 +20,23 @@ class TOdo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
-    return render_template('index.html')
+    if request.method=='POST':
+        todo_title = request.form['title']
+        todo_desc = request.form['desc'] 
+        todo = ToDo(title = todo_title, desc = todo_desc)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = ToDo.query.all()
+    # print(allTodo)
+    return render_template('index.html', allTodo = allTodo)
+
+@app.route("/show")
+def show():
+    allTodo = ToDo.query.all()
+    print(allTodo)
+    return "This is Show page"
 
 if __name__ == "__main__":
     app.run(debug=True)
